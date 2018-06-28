2dd5d7410aaacd9a2b35d5f71b013aadac666e02
Rohith Surya <rohith_gonela@yahoo.com>
2018-06-28 18:11:40 +0530

    Adding profile route using passport js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://surya:gonela15@ds155961.mlab.com:55961/lucifer-on-wheels'
-}
+  mongoURI: 'mongodb://surya:gonela15@ds155961.mlab.com:55961/lucifer-on-wheels',
+  secretOrKey : 'secret'
+};
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+/*jshint esversion: 6 */
+
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -10,12 +14,23 @@ const app = express();
 // DB config
 const db = require('./config/keys').mongoURI;
 
+//Body Parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Connect to MongoDB
 mongoose
   .connect(db)
   .then(()=> console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Sample route
 app.get('/hello', (req, res) => {
   res.send('Hello');
 });

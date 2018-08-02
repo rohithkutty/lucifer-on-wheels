76b991948604c279ea64819b9bb2b15ffaf17d2a
Rohith Surya <rohith_gonela@yahoo.com>
2018-08-02 10:52:59 +0530

    authentication redux fixed

--- client/src/components/layouts/landing-page.js
+++ client/src/components/layouts/landing-page.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 
 class Landing extends Component {
-
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
       this.props.history.push('/dashboard');
@@ -21,7 +20,7 @@ class Landing extends Component {
                 <h1 className="display-3 mb-4">OORA-Connect</h1>
                 <h3>Connecting developers strings</h3>
                 <p className="lead">
-                  {" "}
+                  {' '}
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
                 </p>
@@ -43,10 +42,10 @@ class Landing extends Component {
 
 Landing.propTypes = {
   auth: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = state => ({
   auth: state.auth
-}
+});
 
 export default connect(mapStateToProps)(Landing);

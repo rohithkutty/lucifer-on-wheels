bacfcd67683e7b48e630c5bba6af04c4e02747eb
rohithkutty <rohith_gonela@yahoo.com>
2018-08-03 07:22:31 +0530

    Added private routes to the applicaiton and created Profile routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logOutUser } from './actions/authActions';
@@ -9,53 +9,61 @@ import store from './store';
 
 import './App.css';
 
+import PrivateRoute from './components/common/PrivateRoute';
+
 import Navbar from './components/layouts/navbar';
 import Footer from './components/layouts/footer';
 import Landing from './components/layouts/landing-page';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
 import Dashboard from './components/dashboard/dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 //check for token
-if(localStorage.jwtToken){
-  // set auth token header auth
-  setAuthToken(localStorage.jwtToken);
-  // decode token and get user info
-  const decoded = jwtDecode(localStorage.jwtToken);
-  // set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
+if (localStorage.jwtToken) {
+    // set auth token header auth
+    setAuthToken(localStorage.jwtToken);
+    // decode token and get user info
+    const decoded = jwtDecode(localStorage.jwtToken);
+    // set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
 
-  //check for expired token
-  const currentTime = Date.now()/1000;
-  if(decoded.exp < currentTime){
-    //logout user
-    store.dispatch(logOutUser());
-    //clear current profile
-    store.dispatch(clearCurrentProfile());
-    //redirect to login again
-    window.location.href = '/login'
-  }
+    //check for expired token
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+        //logout user
+        store.dispatch(logOutUser());
+        //clear current profile
+        store.dispatch(clearCurrentProfile());
+        //redirect to login again
+        window.location.href = '/login'
+    }
 }
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Route exact path='/' component={Landing} />
-            <div className='container'>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/dashboard' component={Dashboard} />              
-            </div>
-            <Footer />
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar />
+                        <Route exact path='/' component={Landing} />
+                        <div className='container'>
+                            <Route exact path='/register' component={Register} />
+                            <Route exact path='/login' component={Login} />
+                            <Switch>
+                                <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+                            </Switch>
+                        </div>
+                        <Footer />
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/spinner';
@@ -14,13 +15,27 @@ class Dashboard extends Component {
     const { user } = this.props.auth;
     const { profile, loading } = this.props.profile;
 
-    let dashboardContent =<Spinner />;
+    let dashboardContent;
 
-    // if (profile === null || loading) {
-    //   dashboardContent = <Spinner />
-    // } else {
-    //   dashboardContent = <h1>Hello</h1>
-    // }
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />
+    } else {
+      // Check if logged in user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>TODO: DIsplay profile</h4>
+      } else {
+        // user is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcome {user.name}</p>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+          </Link>
+          </div>
+        )
+      }
+    }
 
     return (
       <div className='dashboard'>
